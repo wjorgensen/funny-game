f34ba1252dfa3bc1be678d8a68aabc8bc675f01e
Wes <wjorgensen29@gmail.com>
2023-11-18 18:53:34 -0500

    lobby styling

--- funny-game-next/layout/layout.tsx
+++ funny-game-next/layout/layout.tsx
@@ -2,5 +2,13 @@ import Footer from "./footer/footer";
 import Navbar from "./navbar/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+        rel="stylesheet"
+      ></link>
+      {children}
+    </>
+  );
 }
