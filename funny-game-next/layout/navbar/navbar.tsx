b8426571be7f0f670a831d42fd079fe8b33b4ea8
s-alad <n7.saad@gmail.com>
2023-11-18 14:14:07 -0500

    add head

--- funny-game-next/layout/navbar/navbar.tsx
+++ funny-game-next/layout/navbar/navbar.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
+
 export default function Navbar() {
     return (
         <>
+            <Head>
+                <title>funny game</title>
+                <meta name="description" content="funny game" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <h1>navbar</h1>
         </>
     )
