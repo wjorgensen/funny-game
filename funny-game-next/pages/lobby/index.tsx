5d97279e4d6693bc7cc8dfb464449e11861998b4
Wes <wjorgensen29@gmail.com>
2023-11-18 15:36:45 -0500

    Lobby

--- funny-game-next/pages/lobby/index.tsx
+++ funny-game-next/pages/lobby/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-
 export default function Lobby() {
   return (
     <>
-        <h1>lobby</h1>
+      <h1>Lobby</h1>
+      <h1>Participants</h1>
+      <div>
+        <ul>
+          <li>Partcipant 1</li>
+          <li>Partcipant 2</li>
+          <li>Partcipant 3</li>
+        </ul>
+      </div>
+      <h1>
+        Room
+        <br />
+        Code
+      </h1>
+      <div>
+        <h2>ABCD</h2>
+      </div>
+      <button>Play Game</button>
     </>
-  )
+  );
 }
