42f4dbda85158ad9c2a66bb81aca94b0bf3f8990
Wes <wjorgensen29@gmail.com>
2023-11-18 16:20:01 -0500

    update lobby css

--- funny-game-next/pages/lobby/index.tsx
+++ funny-game-next/pages/lobby/index.tsx
@@ -1,26 +1,29 @@
 import React from "react";
+import styles from "./lobby.module.scss";
 
 export default function Lobby() {
   return (
     <>
-      <h1>Lobby</h1>
-      <h1>Participants</h1>
-      <div>
-        <ul>
-          <li>Partcipant 1</li>
-          <li>Partcipant 2</li>
-          <li>Partcipant 3</li>
-        </ul>
+      <div className={styles.grid}>
+        <h1 className={styles.header}>Lobby</h1>
+        <h1>Participants</h1>
+        <div>
+          <ul>
+            <li>Partcipant 1</li>
+            <li>Partcipant 2</li>
+            <li>Partcipant 3</li>
+          </ul>
+        </div>
+        <h1>
+          Room
+          <br />
+          Code
+        </h1>
+        <div>
+          <h2>ABCD</h2>
+        </div>
+        <button>Play Game</button>
       </div>
-      <h1>
-        Room
-        <br />
-        Code
-      </h1>
-      <div>
-        <h2>ABCD</h2>
-      </div>
-      <button>Play Game</button>
     </>
   );
 }
