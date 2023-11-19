4c0dc9530b157682519c78e0c361cc119ebd0847
Collin <collinjbarber@gmail.com>
2023-11-18 20:51:46 -0500

    pretty front end for home page

--- funny-game-next/pages/index.tsx
+++ funny-game-next/pages/index.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 import Link from "next/link";
 import { setIn } from "immutable";
 
+const images = [
+  'dragtit.png', 'mariof1.png', 'prplan.png',
+  'shrek-waffle.png', 'sonicss.png', 'spbob.png',
+  'squidd.png', 'deathfry.png', 'leakboat.png'
+];
+
 export default function Home() {
   const [socket, setSocket] = useState<Socket | null | any>(null);
   const [roomId, setRoomId] = useState<string>("");
@@ -89,98 +95,50 @@ export default function Home() {
   };
 
   if (!isStarted && !isInLobby) {
-    return (
-      <>
-        <style jsx global>{`
-          @import url("https://fonts.googleapis.com/css2?family=Jockey+One&display=swap");
+      return (
+    <>
+      <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css2?family=Jockey+One&display=swap');
+        body {
+          background-color: #011134;
+          font-family: 'Jockey One', sans-serif;
+        }
+      `}</style>
 
-          body {
-            background-color: #011134;
-            font-family: "Jockey One", sans-serif;
-          }
-        `}</style>
+      <nav className={styles.navbar}>
+        <ul>
+          <li><Link href="/" className={styles.navLink}>Home</Link></li>
+          <li><Link href="/how-to-play" className={styles.navLink}>How to Play</Link></li>
+          <li><Link href="/gallery" className={styles.navLink}>Gallery</Link></li>
+          <li><Link href="/faqs" className={styles.navLink}>FAQs</Link></li>
+          <li><Link href="/about-us" className={styles.navLink}>About Us</Link></li>
+          <li><Link href="/privacy-terms" className={styles.navLink}>Privacy Terms</Link></li>
+          <li><Link href="/blog" className={styles.navLink}>Blog</Link></li>
+        </ul>
+      </nav>
 
-        <nav className={styles.navbar}>
-          <ul>
-            <li>
-              <Link href="/" className={styles.navLink}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/how-to-play" className={styles.navLink}>
-                How to Play
-              </Link>
-            </li>
-            <li>
-              <Link href="/gallery" className={styles.navLink}>
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link href="/faqs" className={styles.navLink}>
-                FAQs
-              </Link>
-            </li>
-            <li>
-              <Link href="/about-us" className={styles.navLink}>
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link href="/privacy-terms" className={styles.navLink}>
-                Privacy Terms
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog" className={styles.navLink}>
-                Blog
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <header className={styles.header}>
+        <img src="/funny-game.png" alt="Funny Game Logo" className={styles.logo} />
+        <button className={styles.playButton}>Play Me!</button>
+        <p className={styles.tagline}>Unleash Your Imagination with AI-generated Art!</p>
+      </header>
 
-        <header className={styles.header}>
-          <img
-            src="/funny-game.png"
-            alt="Funny Game Logo"
-            className={styles.logo}
-          />
-          <button className={styles.playButton}>Play Me!</button>
-          <p className={styles.tagline}>
-            Unleash Your Imagination with AI-generated Art!
-          </p>
-        </header>
+      <section className={styles.photoContainerGrid}>
+        {Array.from({ length: 7 }).map((_, index) => (
+          <div key={index} className="photo-container">{/* content */}</div>
+        ))}
+      </section>
 
-        <section className={styles.photoContainerGrid}>
-          {Array.from({ length: 7 }).map((_, index) => (
-            <div key={index} className="photo-container">
-              {/* content */}
-            </div>
-          ))}
-        </section>
-
-        <div>
-          <div>
-            <button onClick={() => socket?.emit("createRoom")}>
-              Create Room
-            </button>
-            <button onClick={joinRoom}>Join Room</button>
-            <button onClick={startRoom}>Start Room</button>
+      <section className={styles.photoContainerGrid}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.photoContainer}>
+            {/* Update the src to point to the images folder */}
+            <img src={`/fg-ai-photos/${image}`} alt={image.split('.')[0]} className={styles.photo} />
           </div>
-          {roomId && <p>Room ID: {roomId}</p>}
-          {name && <p>Your Name: {name}</p>}
-          <div>
-            <h2>Connected Users:</h2>
-            <ul>
-              {connectedUsers.map((user) => (
-                <li key={user}>{user}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </>
-    );
+        ))}
+      </section>
+    </>
+  )
   }
 
   if (isInLobby) {
